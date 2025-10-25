fd5a4c2aa3684bdc16b0ea645515a6bc74f3d92a
shruthirgukt <114377385+shruthirgukt@users.noreply.github.com>
2025-10-25 17:27:02 +0530

    Update api.jsx

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -1,7 +1,8 @@
-const backendUrl = "http://localhost:3000";
-// const backendUrl = "https://nutrition-backend-gamma.vercel.app";
+//const backendUrl = "http://localhost:3000";
+const backendUrl = "https://nutri-balance-three.vercel.app";
 
 const userUrl = `${backendUrl}/api/user`;
 const adminUrl = `${backendUrl}/api/admin`;
 
 export { backendUrl, userUrl, adminUrl };
+
